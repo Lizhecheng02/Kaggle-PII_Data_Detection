2ec48f5f4452cd4b8574aaa3dac56f55c181c485
unknown <2569618533@qq.com>
2024-03-23 21:32:16 -0700

    update

--- llm_finetune/pp-dataset.py
+++ llm_finetune/pp-dataset.py
@@ -24,26 +24,34 @@ print(df["label"].value_counts())
 
 df_student_name = df[df["label"] == "student_name"]
 df_other_name = df[df["label"] == "other_name"]
+df_student_name = df_student_name.sample(len(df_other_name))
+df_not_name = df[df["label"] == "not_name"]
 df = pd.concat([df_student_name, df_other_name], ignore_index=True)
+df.reset_index(drop=True, inplace=True)
 print(df.shape)
 print(df["label"].value_counts())
 
 texts = []
 system_prompt = """
-You are an expert at recogonizing student names from long texts. Now here is a long text written by a student:
+Suppose you are an expert at recogonizing student names in the long texts. Here is a long text written by a student:
 """
 question_prompt = """
-The question is that you can easily find {} in the above text, so is {} a student name in the text?
+You can easily find {} in the above text, so is {} a student name in the text? Think step by step, only return 'Yes' or 'No', do not explain the reason.
 """
 for idx, row in tqdm(df.iterrows(), total=len(df)):
     full_text = row["full_text"]
     entity = row["entity"]
     label_output = row["label output"]
+    label = row["label"]
 
-    text = "<s>[INST]" + system_prompt + "\n" + full_text + "\n" + question_prompt.format(entity, entity) + "[/INST]" + label_output + "</s>"
+    if label == "student_name":
+        text = "<s>[INST]" + system_prompt + "\n" + full_text + "\n" + question_prompt.format(entity, entity) + "[/INST]" + "Yes" + "</s>"
+    elif label == "other_name":
+        text = "<s>[INST]" + system_prompt + "\n" + full_text + "\n" + question_prompt.format(entity, entity) + "[/INST]" + "No" + "</s>"
+    
     if idx == 0:
         print(text)
     texts.append(text)
 
 df["text"] = texts
-df.to_csv("llm_train.csv", index=False)
+df.to_csv("llm_train_single_word_output.csv", index=False)
